749cc631b3866311fa937e5f1e038e5a713b3d3e
Filo01 <git@filippo.sh>
2025-10-17 16:39:39 +0200

    feat: docusaurus algolia search

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -163,9 +163,9 @@ const config = {
       },
       algolia: {
         // The application ID provided by Algolia
-        appId: 'YOUR_APP_ID',
+        appId: '3Y0BV0S84E',
         // Public API key: it is safe to commit it
-        apiKey: 'YOUR_SEARCH_API_KEY',
+        apiKey: '7b817b82c15002bcd233cb8d08c45e77',
         indexName: 'homelab-autoscaler',
         // Optional: see doc section below
         contextualSearch: true,
